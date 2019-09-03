157c196b168797a2dcd703af7646a38c66b0af38
Davi Alves <davi.sousa10@gmail.com>
2019-09-03 01:24:10 -0300

    cleanup starter stuffs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Davizera's blog`,
+    description: `That is my blog and im creating it to learn more about gatsby and put m plans in real life! Addicted to coffe <3`,
+    author: `@Davizera`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
