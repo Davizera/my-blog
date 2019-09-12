98c5aeeab518f52a0759287b8695d105a7e79f53
Davi Alves <davi.sousa10@gmail.com>
2019-09-11 21:56:51 -0300

    created styled link inside profile, i still need to import into profile component

--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 
 export const ProfileWrapper = styled.section`
   color: #8899a6;
   display: flex;
   flex-direcion: column;
+`
+export const ProfileLink = styled(Link)`
+  color: #8899a6;
+  text-decoration: none;
+  transation: color 0.7s;
+
+  &hover {
+    color: #1fa1f2;
+  }
 `
