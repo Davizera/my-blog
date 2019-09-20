f438e7e65fcb9c570ff5c632d594cbefe39a5f84
Davi Alves <davi.sousa10@gmail.com>
2019-09-20 19:45:29 -0300

    Fixed 'import' error

--- src/components/SocialLinks/Icons.js
+++ src/components/SocialLinks/Icons.js
@@ -1,13 +1,13 @@
 import { Github } from 'styled-icons/boxicons-logos/Github'
 import { Twitter } from 'styled-icons/boxicons-logos/Twitter'
 import { Instagram } from 'styled-icons/boxicons-logos/Instagram'
-import { Linkedin } from 'styled-icons/boxicons-logos/Linkedin'
+import { Linkedin as LinkedIn } from 'styled-icons/boxicons-logos/Linkedin'
 
 const Icons = {
   Github,
   Twitter,
   Instagram,
-  Linkedin,
+  LinkedIn,
 }
 
 export default Icons
