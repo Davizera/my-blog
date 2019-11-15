bc9cfbd3c3cdfb31e246b1e1d93ad80afeb6339b
Davi Alves <davi.sousa10@gmail.com>
2019-11-15 11:23:44 -0300

    Starting to create search page and setting up algolia

---
+++ src/components/RecommendPosts/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import * as S from './styled'
+
+const RecommendedPost = ({ next, previous }) => (
+  <S.RecommendedWrapper>
+      {!!previous && (
+          
+      )}
+  </S.RecommendedWrapper>
+)
+
+export default RecommendedPost
