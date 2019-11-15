fe5681fbfd1d3527b58dacef623959a320872af5
Davi Alves <davi.sousa10@gmail.com>
2019-11-15 11:56:43 -0300

    Creatind search page component

---
+++ src/pages/search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+
+const SearchPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>You are in the search page</h1>
+  </Layout>
+)
+
+export default SearchPage
