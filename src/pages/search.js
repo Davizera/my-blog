f4635ad1d17e4e15f23375a53f6c6fa9c527c454
Davi Alves <davi.sousa10@gmail.com>
2020-01-01 10:15:14 -0300

    Added: search component

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,11 +1,13 @@
 import React from 'react'
+
 import Layout from '../components/Layout'
+import Search from '../components/Search'
 import SEO from '../components/seo'
 
 const SearchPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>You are in the search page</h1>
+    <Search />
   </Layout>
 )
 
